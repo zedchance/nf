5610ce7a4790f41e9cf25309e753f8cc1e405d0d
Zed Chance <zchance@csus.edu>
2021-04-30 09:08:13 -0700

    Read in entire file

--- nf.c
+++ nf.c
@@ -4,31 +4,45 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/stat.h>
 
 int main(int argc, char ** argv)
 {
+    // usage
+    if (argc < 2)
+    {
+        printf("Usage: %s filename\n", argv[0]);
+        return 1;
+    }
+
     // tape and head
     int tape[30000] = {0};
     int * head = &tape[0];
 
-    if (argc < 2)
-    {
-        fprintf(stderr, "Usage: %s filename\n", argv[0]);
-        exit(1);
-    }
-
+    // open file
     FILE * file = fopen(argv[1], "r");
     if (!file)
     {
-        fprintf(stderr, "Can't open %s for reading\n", argv[1]);
+        printf("Can't open %s for reading\n", argv[1]);
     }
 
-    // read file char by char
-    char c;
-    while ((c = fgetc(file)) != EOF)
+    // get file size
+    struct stat st;
+    int file_size = 0;
+    if (stat(argv[1], &st) == 0)
     {
-        switch(c)
+        file_size = st.st_size;
+    }
+
+    // read in file
+    char source[file_size];
+    fread(source, 1, file_size, file);
+
+    // parse source char by char
+    for (int i = 0; i < file_size; i++)
+    {
+        printf("%c", source[i]);
+        switch(source[i])
         {
             case '+':
                 ++*head;
@@ -42,15 +56,15 @@ int main(int argc, char ** argv)
             case '<':
                 --head;
                 break;
-            case '[':
-                break;
-            case ']':
-                break;
             case ',':
                 scanf("%d", head);
                 break;
             case '.':
                 printf("%d", *head);
+                break;
+            case '[':
+                break;
+            case ']':
                 break;
             default:
                 // skip any other char
@@ -58,3 +72,4 @@ int main(int argc, char ** argv)
         }
     }
 }
+
